a1e862bfdd2f5864f7e494edf98e784306ff1e26
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2021-02-02 04:15:56 -0800

    stack almost ready

--- Castle Conquest/MyStack.h
+++ Castle Conquest/MyStack.h
@@ -1,5 +1,6 @@
 #ifndef MYSTACK_BICHBEVIYKEB
 #define MYSTACK_BICHBEVIYKEB
+#include <iostream>
 #include "StackNode.h"
 template <class T>
 class MyStack
@@ -14,14 +15,68 @@ public:
 		temp = new StackNode<T>();
 		if (!temp)
 		{
-			throw "exception";
-			exit(1);
+			throw "\nHeap Overflow";
 		}
 		else
 		{
 			temp->setData(data);
 			temp->setLink(this->top);
 			this->top = temp;
+		}
+	}
+	bool isEmpty()
+	{
+		if (this->top == nullptr) return true;
+		else return false;
+	}
+	int peek()
+	{
+		if (!isEmpty())
+		{
+			return top->getData();
+		}
+		else
+		{
+		}
+	}
+	void pop()
+	{
+		StackNode<T>* temp;
+		if (top == nullptr)
+		{
+			throw("\nStack Underflow");
+		}
+		else
+		{
+			temp = top;
+			top = top->getLink();
+			temp->setLink(nullptr);
+			delete temp;
+			temp = nullptr;
+		}
+	}
+	void display()
+	{
+		StackNode<T>* temp;
+		if (this->top == nullptr)
+		{
+			throw("\nStack Underflow");
+		}
+		else
+		{
+			temp = this->top;
+			while (temp != nullptr)
+			{
+				std::cout << temp->getData() << "-> ";
+				temp = temp->getLink();
+			}
+		}
+	}
+	~MyStack()
+	{
+		while (this->isEmpty() != nullptr)
+		{
+			this->pop;
 		}
 	}
 };
